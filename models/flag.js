db4adfe9057cc31a56f721d8c88027803a5ef9a0
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-08-26 23:30:47 -0400

    - Added property to flag model to hold mock fk to mimic site

--- models/flag.js
+++ models/flag.js
@@ -4,6 +4,10 @@ const moment = require('moment');
 const Schema = mongoose.Schema;
 
 const flagSchema = new Schema({
+  // This will mock the unique ID for a site
+  site_id: {
+    type: Number
+  },
   type: {
     type: String,
     required: true
@@ -16,13 +20,18 @@ const flagSchema = new Schema({
   }
 });
 
+/**
+ * There didn't appear to be a way to get the entire object in a custom
+ * validator in mongoose so the validation for the date will be done in the
+ * pre-validate hook
+ */
 flagSchema.pre('validate', function (next) {
   // Ensure start_date is before end_date
   if (this.start_date && this.end_date) {
-    const startDateUnix = moment(this.start_date);
-    const endDateUnix = moment(this.end_date);
+    const startDate = moment(this.start_date);
+    const endDate = moment(this.end_date);
 
-    if (startDateUnix >= endDateUnix) {
+    if (startDate >= endDate) {
       next(Error('Start Date must be before End Date'));
     }
   }
